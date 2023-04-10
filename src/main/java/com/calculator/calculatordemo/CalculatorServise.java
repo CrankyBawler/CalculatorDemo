058e55c71f36e96a2f0b509bdb9a5d6ebb6c845b
Cranky Bawler <6666lenia6666@mail.ru>
2023-04-10 21:56:29 +0300

    2

--- src/main/java/com/calculator/calculatordemo/CalculatorServise.java
+++ src/main/java/com/calculator/calculatordemo/CalculatorServise.java
@@ -10,23 +10,43 @@ public class CalculatorServise {
     }
 
     public String sum(int num1, int num2) {
-        int sum = num1 + num2;
-        return num1 + " + " + num2 + " = " + sum;
-
+        if (num1 != 0 && num2 !=0) {
+            int sum = num1 + num2;
+            return num1 + " + " + num2 + " = " + sum;
+        } else {
+            return "Ошибка! введите оба числа";
+        }
     }
+
+
     public String minus(int num1, int num2) {
-        int minus = num1 - num2;
-        return num1 + " - " + num2 + " = " + minus;
+        if (num1 != 0 && num2 != 0) {
+            int minus = num1 - num2;
+            return num1 + " - " + num2 + " = " + minus;
+        } else {
+            return "Ошибка! введите оба числа";
+        }
     }
     public String multiply(int num1, int num2) {
+        if (num1 != 0 && num2 != 0) {
         int multiply = num1 * num2;
         return num1 + " * " + num2 + " = " + multiply;
+        } else {
+            return "Ошибка! введите оба числа";
+        }
     }
     public String divide(int num1, int num2) {
-        int divide = num1 / num2;
-        return num1 + " / " + num2 + " = " + divide;
+        if (num2 == 0) {
+            return " На 0 делить нельзя ";
+        } else {
+            if (num1 != 0 && num2 != 0) {
+                int divide = num1 / num2;
+                return num1 + " / " + num2 + " = " + divide;
+            } else {
+                return "Ошибка! введите оба числа";
+            }
+        }
     }
-
 }
 
 
