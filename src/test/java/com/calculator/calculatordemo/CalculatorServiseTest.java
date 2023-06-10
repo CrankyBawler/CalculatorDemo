2ea51afcbf5929090b0cc93dde18d079884035f9
Cranky Bawler <6666lenia6666@mail.ru>
2023-06-10 11:29:52 +0300

    2

--- src/test/java/com/calculator/calculatordemo/CalculatorServiseTest.java
+++ src/test/java/com/calculator/calculatordemo/CalculatorServiseTest.java
@@ -2,70 +2,102 @@ package com.calculator.calculatordemo;
 
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorServiseTest {
     CalculatorServiсe serviсe = new CalculatorServiсe();
+
     @Test
     void testSum() {
         var result = serviсe.sum(1, 2);
         assertEquals(3, result);
 
         var result2 = serviсe.sum(-1, -2);
-        assertEquals(-3, result);
+        assertEquals(-3, result2);
 
         var result3 = serviсe.sum(-1, 2);
-        assertEquals(1, result);
+        assertEquals(1, result3);
 
         var result4 = serviсe.sum(1, -2);
-        assertEquals(1, result);
+        assertEquals(-1, result4);
 
         var result5 = serviсe.sum(0, 2);
-        assertEquals(2, result);
+        assertEquals(2, result5);
 
         var result6 = serviсe.sum(1, 0);
-        assertEquals(1, result);
+        assertEquals(1, result6);
     }
+
     @Test
-    void testMinus () {
+    void testMinus() {
         var result = serviсe.minus(1, 2);
         assertEquals(-1, result);
 
         var result2 = serviсe.minus(-1, -2);
-        assertEquals(1, result);
+        assertEquals(1, result2);
 
         var result3 = serviсe.minus(-1, 2);
-        assertEquals(-3, result);
+        assertEquals(-3, result3);
 
         var result4 = serviсe.minus(1, -2);
-        assertEquals(3, result);
+        assertEquals(3, result4);
 
         var result5 = serviсe.minus(0, 2);
-        assertEquals(-2, result);
+        assertEquals(-2, result5);
 
         var result6 = serviсe.minus(1, 0);
-        assertEquals(1, result);
+        assertEquals(1, result6);
     }
+
     @Test
     void testMultiply() {
         var result = serviсe.multiply(1, 2);
         assertEquals(2, result);
 
         var result2 = serviсe.multiply(-1, -2);
-        assertEquals(2, result);
+        assertEquals(2, result2);
 
         var result3 = serviсe.multiply(-1, 2);
-        assertEquals(-2, result);
+        assertEquals(-2, result3);
 
         var result4 = serviсe.multiply(1, -2);
-        assertEquals(-2, result);
+        assertEquals(-2, result4);
 
         var result5 = serviсe.multiply(0, 2);
-        assertEquals(0, result);
+        assertEquals(0, result5);
 
         var result6 = serviсe.multiply(1, 0);
-        assertEquals(0, result);
+        assertEquals(0, result6);
 
     }
+
+    @Test
+    void testDivide() {
+        var result = serviсe.multiply(2, 1);
+        assertEquals(2, result);
+
+        var result2 = serviсe.multiply(-2, -1);
+        assertEquals(2, result2);
+
+        var result3 = serviсe.multiply(-2, 1);
+        assertEquals(-2, result3);
+
+        var result4 = serviсe.multiply(2, -1);
+        assertEquals(-2, result4);
+
+        var result5 = serviсe.multiply(0, 2);
+        assertEquals(0, result5);
+
+        assertThrows(IllegalArgumentException.class, () -> serviсe.divide(1, 0));
+
+        try {
+            serviсe.divide(1, 0);
+            fail();
+        } catch (IllegalArgumentException ignored) {
+
+        }
+    }
+
 
 }
